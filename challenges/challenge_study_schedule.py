3ab81ea7f729e060d3f018b3ae485436b25c38fa
eon <bernardomp.dev@gmail.com>
2023-07-11 19:51:55 -0300

    refactor: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,8 +2,8 @@ def validate_params(permanence_period, target_time):
     if target_time is None:
         return True
 
-    for time in permanence_period:
-        if type(time[0]) != int or type(time[1]) != int:
+    for start_time, end_time in permanence_period:
+        if type(start_time) != int or type(end_time) != int:
             return True
 
 
@@ -11,15 +11,9 @@ def study_schedule(permanence_period, target_time):
     if validate_params(permanence_period, target_time):
         return None
 
-    target_counter = 0
-
-    for period in permanence_period:
-        average_hour = period[0] + period[1] / len(period)
-
-        if period[0] == target_time or period[1] == target_time:
-            target_counter += 1
-
-        if average_hour == target_time:
-            target_counter += 1
-
-    return target_counter
+    return sum(
+        1 for start_time, end_time in permanence_period
+        if start_time == target_time
+        or end_time == target_time
+        or ((start_time + end_time) // 2) == target_time
+    )
