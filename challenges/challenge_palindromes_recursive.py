dfde02be8e008e4617754dfc1944d49fcaa52840
eon <bernardomp.dev@gmail.com>
2023-07-12 15:01:28 -0300

    refactor: is_palindrome_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,11 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word is None or word == "":
+    if (word == "" or word[low_index] != word[high_index]):
         return False
 
     if low_index >= high_index:
         return True
 
-    if word[low_index] == word[high_index]:
-        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    else:
-        return False
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
