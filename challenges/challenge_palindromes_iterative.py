3f17cede68849ffac409a48388586f7ab847deb9
eon <bernardomp.dev@gmail.com>
2023-07-12 15:01:38 -0300

    feat: is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == "":
+        return False
+
+    if word[::-1] == word:
+        return True
+
+    return False
