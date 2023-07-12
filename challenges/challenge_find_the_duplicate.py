12629f31f7377f1a5aadfae27cbf8662d59bbc04
eon <bernardomp.dev@gmail.com>
2023-07-12 16:02:01 -0300

    feat: find_duplicate.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums is None or len(nums) <= 1:
+        return False
+
+    seen = set()
+
+    for num in nums:
+        if (type(num) != int or num <= 0):
+            return False
+        if num in seen:
+            return num
+        seen.add(num)
+
+    return False
